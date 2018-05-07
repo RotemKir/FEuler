a568043714a336fa2594b9eb679cb4d40e219176
Rotem Kirshenbaum <rotemkir@gmail.com>
2018-05-07 20:38:11 +0200

    Moved problems to single module

--- FEuler.Problems/Problems.fs
+++ FEuler.Problems/Problems.fs
@@ -8,15 +8,15 @@ module Problems =
         Map.ofSeq <|
         Seq.map (fun p -> (string p.Number, p))
             [
-                { Number = 1 ; Solver = Problem1.solve >> int64; Answer = Some 233168L }
-                { Number = 2 ; Solver = Problem2.solve >> int64; Answer = Some 4613732L }
-                { Number = 3 ; Solver = Problem3.solve >> int64; Answer = Some 6857L }
-                { Number = 4 ; Solver = Problem4.solve >> int64; Answer = Some 906609L }
-                { Number = 5 ; Solver = Problem5.solve >> int64; Answer = Some 232792560L }
-                { Number = 6 ; Solver = Problem6.solve >> int64; Answer = Some 25164150L }
-                { Number = 7 ; Solver = Problem7.solve >> int64; Answer = Some 104743L }
-                { Number = 8 ; Solver = Problem8.solve ; Answer = Some 23514624000L }
-                { Number = 9 ; Solver = Problem9.solve >> int64 ; Answer = Some 31875000L }
+                { Number = 1 ; Solver = Problems1To10.solve1 >> int64; Answer = Some 233168L }
+                { Number = 2 ; Solver = Problems1To10.solve2 >> int64; Answer = Some 4613732L }
+                { Number = 3 ; Solver = Problems1To10.solve3 >> int64; Answer = Some 6857L }
+                { Number = 4 ; Solver = Problems1To10.solve4 >> int64; Answer = Some 906609L }
+                { Number = 5 ; Solver = Problems1To10.solve5; Answer = Some 232792560L }
+                { Number = 6 ; Solver = Problems1To10.solve6 >> int64; Answer = Some 25164150L }
+                { Number = 7 ; Solver = Problems1To10.solve7 >> int64; Answer = Some 104743L }
+                { Number = 8 ; Solver = Problems1To10.solve8 ; Answer = Some 23514624000L }
+                { Number = 9 ; Solver = Problems1To10.solve9 >> int64 ; Answer = Some 31875000L }
             ]
 
     let getProblem problemNumber =
